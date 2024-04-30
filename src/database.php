af930637098caaf08a0134400b6452208f44945d
stackJx <root@stackbug.dev>
2024-04-30 15:20:32 +0800

    first commit

---
+++ src/database.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // 默认数据连接标识
+    'default' => 'mysql',
+    // 数据库连接信息
+    'connections' => [
+        'mysql' =>
+            [
+                // 数据库类型
+                'type' => 'mysql',
+                // 服务器地址
+                'hostname' => '172.30.166.27',
+                // 数据库名
+                'database' => 'wxpc',
+                // 用户名
+                'username' => 'wxpc',
+                // 密码
+                'password' => 'fCZDyYeTkYkscYN5',
+                // 端口
+                'hostport' => '3306',
+                // 数据库编码默认采用utf8
+                'charset' => 'utf8',
+                // 数据库调试模式
+                'debug' => true,
+            ],
+    ],
+
+
+];
